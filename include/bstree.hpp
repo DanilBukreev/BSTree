9f6ea77c68c10bac349841de4e376cba36717cb2
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2018-04-20 14:05:55 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,28 +1,34 @@
 namespace BSTree {
 struct Node {
-  int key;
+  int data;
   Node* left;
   Node* right;
+  int value;
 };
 class Tree {
   Node* root;
   void remove(Node*& node);
-  void add_node(int key, Node*& root);
+  void add_node(int data, Node*& root);
   void showTree(Node* node, int field);
   void PreOrderTree(Node* node);
   void InOrderTree(Node* node);
   void PostOrderTree(Node* node);
-  void WriteInFile(Node *root,  std::string name);
+  void WriteInFile(Node* root, std::string name);
+  bool LoadfromfileTree(Node* root);
+  bool Deletenode(Node*& root, int value);
 
  public:
   Tree();
   bool zero();
   void show();
-  void insert(int key);
+  void insert(int data);
   void PreOrder();
   void InOrder();
   void PostOrder();
   void Write();
+  bool Loadfromfile();
+  bool Deleten(int value);
+
   ~Tree();
 };
 }
